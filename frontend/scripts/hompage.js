5fbaa741a9dca1ac20a5fa921a4d8f6f74ca6866
CALEB <owatahcaleb@gmail.com>
2022-12-14 20:12:42 +0100

    input field

--- frontend/scripts/hompage.js
+++ frontend/scripts/hompage.js
@@ -1,1 +1,31 @@
-console.log('chunk_file');
+// console.log('chunk_file');
+
+
+ // grabbed the input field
+ const droparea = document.querySelector('.file');
+   console.log(droparea)
+
+   droparea.addEventListener('dragover', (e) => {
+     e.preventDefault();
+   });
+
+   droparea.addEventListener('drop', (e) => {
+     e.preventDefault();
+
+     const file = e.dataTransfer.files[0];
+     let type= file.type
+     console.log(type);
+
+     if (type === 'text/csv') {
+         return upload(file);
+     } else {
+         droparea.setAttribute('class', 'droparea inalid');
+         droparea.innerHTML = 'Invalid file format..!';
+         return false;
+     }
+
+     const upload = (file) => {
+         droparea.setAttribute('class', 'droparea valid');
+         droparea.innerHTML = 'Added' + file.name;
+     }
+   });
