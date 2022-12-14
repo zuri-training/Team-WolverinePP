b587dfa797b3cfed3329e6b4468e3162de07fa78
Joshua Onwuemene <emjay6299@gmail.com>
2022-12-14 08:08:40 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,42 +5,49 @@ const secret = "verySecureSECRET";
 const expiryTime = 3600;
 
 exports.registerNewUser = (req, res) => {
-  // fetch user details from body
+  // fetch user details from request body
+const { name, email, username, password } = req.body;
+  
   // check if user with this username exists
-  User.findOne({ username: req.body.username }, (err, existingUser) => {
+  User.findOne({ username }, (err, existingUser) => {
     if (err) {
       return res.status(500).json({ err });
     }
     if (existingUser) {
       return res.status(400).json({ message: "username already exist" });
     }
+    
     // create a new user
     User.create(
       {
-        name: req.body.name,
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password
+        name,
+        email,
+        username,
+        password
       },
       (err, newUser) => {
         if (err) {
           return res.status(500).json({ err });
         }
+        
         // hash user's password
         bcrypt.genSalt(10, (err, salt) => {
           if (err) {
             return res.status(500).json({ err });
           }
-          bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
+          
+          bcrypt.hash(password, salt, (err, hashedPassword) => {
             if (err) {
               return res.status(500).json({ err });
             }
+            
             // save password to db
             newUser.password = hashedPassword;
             newUser.save((err, savedUser) => {
               if (err) {
                 return res.status(500).json({ err });
               }
+              
               // create jwt payload for user
               jwt.sign(
                 {
@@ -55,6 +62,7 @@ exports.registerNewUser = (req, res) => {
                   if (err) {
                     return res.status(500).json({ err });
                   }
+
                   // send token to user
                   return res.status(200).json({
                     message: "Registration successful",
