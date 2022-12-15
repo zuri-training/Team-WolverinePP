2d2ca9a39dbbd3db9501dc99bdde31c4c878f56a
Vctcode <victorode01@gmail.com>
2022-12-15 23:56:01 +0100

    bug fix in controllers

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -1,30 +1,47 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
+const { SECRET } = process.env;
 
-require('dotenv').config();
-const {SECRET} = process.env;
+const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
 
-exports.authenticateUser = (req, res, next) => {
-  // check if there is an authorization token
-  if (!req.headers.authorization) {
-    return res.status(401).json({
-      message: "Authentication is required",
-    });
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
   }
-  let headerSplit = req.headers.authorization;
-
-  if(headerSplit[0] !== "Bearer") {
-    return res.status(401).json({
-        message: "authorization format is Bearer <token>"
-    })
+  try {
+    const decoded = jwt.verify(token, SECRET);
+    req.existingUser = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
   }
-  let token = headerSplit[1];
-//   check validity
-  jwt.verify(token, SECRET, (err, decodedToken) => {
-    if (err) return res.status(500).json({ err })
-    if (!decodedToken) {
-        return res.status(401).json({message: "invalid authorization token, login to continue"})
-    }
-    // allow user to continue
-    next()
-  })
+  return next();
 };
+
+module.exports = verifyToken;
+
+// exports.authenticateUser = (req, res, next) => {
+//   // check if there is an authorization token
+//   if (!req.headers.authorization) {
+//     return res.status(401).json({
+//       message: "Authentication is required",
+//     });
+//   }
+//   let headerSplit = req.headers.authorization;
+
+//   if(headerSplit[0] !== "Bearer") {
+//     return res.status(401).json({
+//         message: "authorization format is Bearer <token>"
+//     })
+//   }
+//   let token = headerSplit[1];
+// //   check validity
+//   jwt.verify(token, SECRET, (err, decodedToken) => {
+//     if (err) return res.status(500).json({ err })
+//     if (!decodedToken) {
+//         return res.status(401).json({message: "invalid authorization token, login to continue"})
+//     }
+//     // allow user to continue
+//     next()
+//   })
+// };
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -3,7 +3,7 @@
 // const path = require("path");
 
 const router = require("express").Router();
-const { authenticateUser } = require("../middlewares/authentication.js");
+const  authenticateUser = require("../middlewares/authentication.js");
 const authController = require("../controllers/authController.js");
 const pageController = require("../controllers/pageController.js");
 const chunkController = require("../controllers/chunkController.js");
@@ -14,9 +14,9 @@ router
   .get("/", pageController.getLandingPage)
   .get("/signup", pageController.getSignUpPage)
   .get("/login", pageController.getLoginPage)
-  .post("/signup", authenticateUser(), authController.registerNewUser)
-  // .post("/login", auth, authController.loginUser)
-  // .post("/library", auth, chunkController.split);
+  .post("/signup", authenticateUser, authController.registerNewUser)
+  .post("/login", authenticateUser, authController.loginUser)
+  // .post("/library", authenticateUser, chunkController.split);
 
 // const AuthController = require("../controllers/authController");
 
