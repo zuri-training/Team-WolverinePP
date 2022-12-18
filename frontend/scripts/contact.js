0779c32baadc99dbce927679a10c509fbc20b0bd
ibukunoluwaM <ibukunoluwaolateju@gmail.com>
2022-12-17 16:10:08 -0800

    added files for the contact page

---
+++ frontend/scripts/contact.js
@@ -0,0 +1,9 @@
+function iconStuff() {
+    let x = document.getElementById("list1");
+
+    if(x.style.display ==="block") {
+        x.style.display ="none";
+    } else {
+        x.style.display ="block";
+    }
+}
